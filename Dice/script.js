7248a583ae51f1e90b92864070ecf034d4d8bd05
Doit <duhee7524@naver.com>
2023-11-15 14:25:12 +0900

    주사위 게임 완성.

--- Dice/script.js
+++ Dice/script.js
@@ -1,30 +1,71 @@
-// script.js
-
-const player1 = document.querySelector('.player--1');
-const player2 = document.querySelector('.player--2');
-const score1 = document.querySelector('.player--1 .score');
-const score2 = document.querySelector('.player--2 .score');
-const currentScore1 = document.querySelector('.player--1 .current-score');
-const currentScore2 = document.querySelector('.player--2 .current-score');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
+const btnNew = document.querySelector('.btn--new');
+const dcieNum = document.querySelector('.diceNum');
 
-let activePlayer = 1;
-let currentPlayerScore = 0;
-let totalScore1 = 0;
-let totalScore2 = 0;
+let curPlayer  = 1;
+let curPlayerScore = 0;
+let curPlayerScoreElement;
+let curPlayerElement;
 
+var playInit = function(){
+    curPlayerScoreElement.textContent =0;
+    curPlayerScore=0;
+    curPlayerElement.style.backgroundColor ="#AD3047";
+}
+
+const rollDice =() => {
+    curPlayerScoreElement =document.querySelector(`.player--${curPlayer} .current-score`)
+    curPlayerElement =document.querySelector(`.player--${curPlayer}`)
+    curPlayerElement.style.backgroundColor = "green";
+
+    const diceValue = getRandomValue();
+    dcieNum.textContent = diceValue;
+
+    if(diceValue === 1 || diceValue === 2){
+        playInit();
+        curPlayer = (curPlayer === 1) ? 2 : 1;
+        curPlayerElement =document.querySelector(`.player--${curPlayer}`)
+        curPlayerElement.style.backgroundColor = "green";
+        console.log("확인");
+    }
+    else{
+        curPlayerScore += diceValue;
+        curPlayerScoreElement.textContent =curPlayerScore;
+        if(curPlayerScore >50){
+            dcieNum.textContent = `player${curPlayer} 숭리`
+            curPlayerScore =0;
+        }
+        
+    }
+
+};
+const holdDice =() => {
+    if(curPlayerScore>50){
+        dcieNum.textContent = `player ${curPlayer} 숭리`
+    }
+    else{
+        playInit();
+        curPlayer = (curPlayer === 1) ? 2 : 1;
+        curPlayerElement =document.querySelector(`.player--${curPlayer}`)
+        curPlayerElement.style.backgroundColor = "green";
+    }
+}
+
+const newStart =()=>{
+    curPlayer  = 1;
+    playInit();
+    curPlayerElement.style.backgroundColor = "green";
+}
+
+
+// 주사위를 굴려서 나온 값을 랜덤하게 설정
 function getRandomValue(){
     return Math.floor(Math.random() * 6) + 1;
 }
 
-function rollDice() {
-    // 주사위를 굴려서 나온 값을 랜덤하게 설정
-    const diceValue = getRandomValue();
-    // 현재 플레이어의 현재 스코어에 주사위 값 추가
-    currentPlayerScore += diceValue;
-    currentScore1.textContent = currentPlayerScore;
-}
 
 btnRoll.addEventListener("click", rollDice);
+btnHold.addEventListener("click", holdDice);
+btnNew.addEventListener("click", newStart);
 
